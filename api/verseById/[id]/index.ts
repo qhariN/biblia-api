71afad9a7e245934f6949be13f1f8ff2cade000e
David Rey <neisserrey@upeu.edu.pe>
2023-12-14 20:05:33 -0500

    feat: :sparkles: find verses by Id
    Find verses by id and cromo start instead of listen :)

---
+++ api/verseById/[id]/index.ts
@@ -0,0 +1,15 @@
+import Database from "bun:sqlite";
+import { CromoHandler } from "cromo";
+
+export const GET: CromoHandler = ({ params, responseInit }) => {
+  const verseId = params.id.toUpperCase();
+
+  const db = new Database("./src/database/bible.db");
+  const query = db.prepare(
+    `SELECT id, number, reference, content, chapterId FROM verse WHERE id = ?1`
+  );
+
+  const verse = query.get(verseId) as Object;
+
+  return Response.json(verse, responseInit);
+};
