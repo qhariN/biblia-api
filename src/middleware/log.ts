8d614f06f47adcdeacd8f0e59bda1f9fbbf9bca3
Jhorman <jhormanrus@gmail.com>
2024-03-26 04:29:45 +0000

    ✨ add datetime in log

--- src/middleware/log.ts
+++ src/middleware/log.ts
@@ -3,8 +3,12 @@ import { CromoMiddleware } from 'cromo'
 export const log: CromoMiddleware = async (context, next) => {
   const { request, url } = context
   const response = await next(context)
-  console.log(
-    `${request.method.padStart(7)}:${response.status} -> ${url.pathname}`,
-  )
+
+  const datetime = new Date().toISOString().replace('T', ' ').replace('Z', '')
+  const method = request.method.padStart(7)
+  const status = response.status
+  const path = url.pathname
+  console.log(`${datetime} ${method}:${status} -> ${path}`)
+
   return response
 }
